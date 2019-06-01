45aefa937ba8f8a33b4aa41d03a2c2ac23eb8145
Lucas Teske <lucas@teske.net.br>
2019-06-01 12:58:33 -0300

    Added interactivity to GameEditor (to change Server IP)

--- cmd/gameEditor/gameEditor.go
+++ cmd/gameEditor/gameEditor.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"github.com/HUEBRTeam/PrimeServer/tools"
 	"github.com/quan-to/slog"
+	"io"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 var log = slog.Scope("GameEditor")
 
 func main() {
-	data, err := ioutil.ReadFile("piu_108o_original")
+	reader := bufio.NewReader(os.Stdin)
+
+	log.Info("Please type the filename to edit: ")
+
+	filename, err := reader.ReadString('\n')
+
+	if err != nil {
+		if err != io.EOF {
+			log.Fatal(err)
+		}
+		os.Exit(1)
+	}
+
+	filename = strings.Trim(filename, "\r\n ")
+
+	if !tools.Exists(filename) {
+		log.Fatal("File %s does not exists", filename)
+	}
+
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,11 +53,60 @@ func main() {
 
 	log.Info("IP Found at %d: %s", o, ip)
 
-	if !SetPrimeServerIP(data, "8.8.8.8") {
-		log.Fatal("Your IP does not fit into Prime memory :(")
+	sl := true
+	for sl {
+		log.Info("Please enter the IP you want to write: ")
+		ip, err := reader.ReadString('\n')
+
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			os.Exit(1)
+		}
+
+		ip = strings.Trim(ip, "\r\n ")
+
+		if !tools.IsValidIP4(ip) {
+			log.Error("The IP Address \"%s\" is not valid.", ip)
+			continue
+		}
+
+		log.Info("Do you want to set the IP %s to game?", ip)
+
+		r, _ := reader.ReadString('\n')
+
+		r = strings.ToLower(r)
+
+		if len(r) == 0 || (r[0] != 's' && r[0] != 'y') {
+			continue
+		}
+
+		if !SetPrimeServerIP(data, ip) {
+			log.Error("Your IP does not fit into Prime memory :(")
+			continue
+		}
+
+		o, ip = FindPrimeServerIP(data)
+
+		log.Info("Current IP: %s", ip)
+		log.Info("I will save the file. That's correct?")
+
+		r, _ = reader.ReadString('\n')
+		r = strings.ToLower(r)
+
+		if len(r) == 0 || (r[0] != 's' && r[0] != 'y') {
+			continue
+		}
+
+		err = ioutil.WriteFile(filename, data, os.ModePerm)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		sl = false
 	}
 
-	o, ip = FindPrimeServerIP(data)
-
-	log.Info("IP Found at %d: %s", o, ip)
+	log.Info("Done!")
 }
---
+++ tools/file.go
@@ -0,0 +1,30 @@
+package tools
+
+import "os"
+
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsDir(name string) bool {
+	s, err := os.Stat(name)
+	if err != nil {
+		return false
+	}
+
+	return s.IsDir()
+}
+
+func IsFile(name string) bool {
+	s, err := os.Stat(name)
+	if err != nil {
+		return false
+	}
+
+	return !s.IsDir()
+}
